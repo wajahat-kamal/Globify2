4201a71196663391494e3aaa07fab42d8f0a56a8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-11 10:29:16 +0500

    Refactor project structure by removing the frontend-2 directory and all related files, including configurations, components, pages, and assets.

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,105 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+    try {
+        const { firstName, lastName, email,  password } = req.body;
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email"
+            });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 6 characters"
+            });
+        }
+
+        const existingUserByEmail = await User.findOne({ email: email });
+
+        if (existingUserByEmail) {
+            return res.status(400).json({ success: false, message: "Email already exists" });
+        }
+
+        // const existingUserByUsername = await User.findOne({ userName: userName });
+
+        // if (existingUserByUsername) {
+        //     return res.status(400).json({ success: false, message: "Username already exists" });
+        // }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        })
+
+        return res.status(201).json({
+            success: true,
+            message: "Account Created Successfully"
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to register"
+        })
+
+    }
+}
+
+export const login = async(req, res) => {
+    try {
+        const {email,  password } = req.body;
+        if (!email && !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect email or password"
+            })
+        }
+       
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(400).json({ 
+                success: false, 
+                message: "Invalid Credentials" 
+            })
+        }
+        
+        const token = await jwt.sign({userId:user._id}, process.env.SECRET_KEY, { expiresIn: '1d' })
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: "strict" }).json({
+            success:true,
+            message:`Welcome back ${user.firstName}`,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to Login",           
+        })
+    }
+  
+}
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./database/db.js"
+import userRoute from "./routes/user.route.js"
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: "https://mern-blog-ha28.onrender.com",
+    credentials:true
+}))
+
+
+// apis
+ app.use("/api/v1/user", userRoute)
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server listen at port ${PORT}`);
+    connectDB()
+})
