7abed55479441f2aaeef2497cbb05b4bfd08e20f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-17 22:17:17 +0500

    Add route for updating user profile with authentication and file upload middleware

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/user.controller.js"
+import { login, logout, signup, updateProfile } from "../controllers/user.controller.js"
+import { isAuthenticated } from "../middleware/isAuthenticated.js"
+import { singleUpload } from "../middleware/multer.js"
 const router = express.Router()
 
 router.route("/signup").post(signup)
 router.route("/login").post(login)
 router.route("/logout").get(logout)
+router.route("/profile/update").put(isAuthenticated, singleUpload, updateProfile)
 
 export default router;
