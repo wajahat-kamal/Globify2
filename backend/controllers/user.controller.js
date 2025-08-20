08f8d4b34e3a0ffc4547a702d437a57e7459f41e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-20 12:15:35 +0500

    Refactor user controller to integrate Cloudinary for image handling; update data URI utility for correct file name property, enhancing image upload functionality.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/datauri.js";
-import { response } from "express";
+import cloudinary from "../utils/cloudinary.js";
 
 export const signup = async (req, res) => {
   try {
--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -1,11 +1,11 @@
-import dataUriParser from "datauri/parser.js"
-import path from "path"
+import dataUriParser from "datauri/parser.js";
+import path from "path";
 
 const parser = new dataUriParser();
 
 const getDataUri = (file) => {
-    const extName = path.extname(file.orignalname).toString();
-    return parser.format(extName, file.buffer).content
-}
+  const extName = path.extname(file.originalname).toString();
+  return parser.format(extName, file.buffer).content;
+};
 
 export default getDataUri;
