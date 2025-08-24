4367856b1ebf9946f231726332584284b5498c60
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-24 20:50:43 +0500

    Update CORS configuration in server setup to use environment variable for client URL instead of hardcoded value.

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
